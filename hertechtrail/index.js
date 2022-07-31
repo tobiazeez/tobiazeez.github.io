0120774ccd910fa4d18d1c6d7043c76cf3528d6f
Tobi Azeez <azeez.oluwatobi@stu.cu.edu.ng>
2022-07-31 21:34:15 +0100

    Add files via upload

---
+++ hertechtrail/index.js
@@ -0,0 +1,62 @@
+const highlight = document.getElementById("img1");
+
+highlight.addEventListener("click", (event)=>{
+    event.target.style.background = "Brown";
+    event.target.style.color = "white";
+})
+
+const highlight2 = document.getElementById("img2");
+
+highlight2.addEventListener("click", (event)=>{
+    event.target.style.background = "black";
+    event.target.style.color = "grey";
+})
+
+const highlight3 = document.getElementById("img3");
+
+highlight3.addEventListener("click", (event)=>{
+    event.target.style.background = "purple";
+    event.target.style.color = "lightblue";
+})
+
+const highlight4 = document.getElementById("img4");
+
+highlight4.addEventListener("click", (event)=>{
+    event.target.style.background = "blue";
+    event.target.style.color = "pink";
+})
+
+const highlight5 = document.getElementById("img5");
+
+highlight5.addEventListener("click", (event)=>{
+    event.target.style.background = "red";
+    event.target.style.color = "pink";
+})
+
+const highlight6 = document.getElementById("img6");
+
+highlight6.addEventListener("click", (event)=>{
+    event.target.style.background = "darkgrey";
+    event.target.style.color = "yellow";
+})
+
+const highlight7 = document.getElementById("img7");
+
+highlight7.addEventListener("click", (event)=>{
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+})
+
+const highlight8 = document.getElementById("img8");
+
+highlight8.addEventListener("click", (event)=>{
+    event.target.style.background = "orange";
+    event.target.style.color = "white";
+})
+
+const highlight9 = document.getElementById("img9");
+
+highlight9.addEventListener("click", (event)=>{
+    event.target.style.background = "darkblue";
+    event.target.style.color = "lightgrey";
+})
